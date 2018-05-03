b81ed978e3ca34b17fb8ac8adfcb93bb4c28ab88
Saurabh Agarwal <shourabh.agarwal@gmail.com>
2018-05-03 13:27:51 +0530

    Run collectstatic on manage.py

--- viscom/settings.py
+++ viscom/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
-from mood_detection.utils.inference import load_detection_model
-from keras.models import load_model
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
